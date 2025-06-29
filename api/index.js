9bbb8ee0626e9c90fbe53756fbb65786528d4358
Muhammad-Anas16 <anas.hanf38@gmail.com>
2025-06-30 01:21:12 +0500

    put index.js in api folder

--- api/index.js
+++ api/index.js
@@ -2,9 +2,9 @@ import express from 'express';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import 'dotenv/config';
-import taskRoute from './Routes/task.js';
-import authRoute from './Routes/auth.js';
-import userRouter from './Routes/user.js';
+import taskRoute from '../Routes/task.js';
+import authRoute from '../Routes/auth.js';
+import userRouter from '../Routes/user.js';
 
 const app = express();
 
